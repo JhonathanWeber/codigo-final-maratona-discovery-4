30dfa8c41f8d065e5468723ffc088d544ce34dd8
Jhonathan Weber <jhonmw@gmail.com>
2021-04-10 19:37:39 -0300

    :Project DevFinance$: Correcao no formatAmount()

--- scripts.js
+++ scripts.js
@@ -120,8 +120,8 @@ const Utils = {
     return signal + value;
   },
   formatAmount(value) {
-    value = Number(value) * 100;
-    return value;
+    value = value * 100;
+    return Math.round(value);
   },
   formatDate(date) {
     let splittedDate = date.split("-");
